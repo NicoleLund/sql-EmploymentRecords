1af66217ffa97244e32f1ec2d3cd25b6c2af7a51
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-05-10 11:05:17 -0700

    Data Modeling

---
+++ EmployeeSQL/2_SQL_model/QuickDBD-employee_schema.sql
@@ -0,0 +1,91 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/3rRTV7
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Data Analytics 09-SQL homework
+-- Create Employee Database Model
+
+-- Identifies a unique name for each department
+CREATE TABLE "departments" (
+    "dept_no" varchar(10)   NOT NULL,
+    "dept_name" varchar(25)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     ),
+    CONSTRAINT "uc_departments_dept_name" UNIQUE (
+        "dept_name"
+    )
+);
+
+-- Identifies the department assignment of employees.
+-- Note: Some employees are assigned to multiple departments.
+CREATE TABLE "dept_employees" (
+    "emp_no" varchar(10)   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_dept_employees" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+-- Identifies the department managed by an employee.
+CREATE TABLE "dept_managers" (
+    "emp_no" varchar(10)   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_dept_managers" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Records employee demographics.
+CREATE TABLE "employees" (
+    "emp_no" varchar(10)   NOT NULL,
+    "first_name" varchar(25)   NOT NULL,
+    "last_name" varchar(25)   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "hire_date" date   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Identifies the salary for employees.
+CREATE TABLE "salaries" (
+    "emp_no" varchar(10)   NOT NULL,
+    "salary" integer   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Identifies the unique name for each job title.
+CREATE TABLE "titles" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(25)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     ),
+    CONSTRAINT "uc_titles_title" UNIQUE (
+        "title"
+    )
+);
+
+ALTER TABLE "dept_employees" ADD CONSTRAINT "fk_dept_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_employees" ADD CONSTRAINT "fk_dept_employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "employees" ("emp_title_id");
+
