420d237170af204b4921b75ea37661ac532af600
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-05-11 10:09:53 -0700

    Data Analysis Questions 5-8

--- EmployeeSQL/4_SQL_Analysis/query.sql
+++ EmployeeSQL/4_SQL_Analysis/query.sql
@@ -76,22 +76,62 @@ SELECT de.emp_no,
   
 ------------------------------------
 -- 5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
-
+SELECT e.first_name,
+       e.last_name,
+       e.sex
+  FROM employees AS e 
+ WHERE e.first_name = 'Hercules'
+   AND e.last_name LIKE 'B%';
   
   
 ------------------------------------
 -- 6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
-
+SELECT e.emp_no,
+       e.last_name,
+       e.first_name,
+       d.dept_name 
+  FROM employees AS e 
+       INNER JOIN dept_employees AS de
+       ON e.emp_no = de.emp_no 
+       INNER JOIN departments d 
+       ON de.dept_no = d.dept_no 
+ WHERE d.dept_name = 'Sales';
   
   
 ------------------------------------
--- 7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+-- 7a.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no,
+       e.last_name,
+       e.first_name,
+       d.dept_name 
+  FROM employees AS e 
+       INNER JOIN dept_employees AS de
+       ON e.emp_no = de.emp_no 
+       INNER JOIN departments d 
+       ON de.dept_no = d.dept_no 
+ WHERE d.dept_name = 'Sales'
+   AND d.dept_name = 'Development';
 
   
+------------------------------------
+-- 7b.List all employees in the Sales OR Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no,
+       e.last_name,
+       e.first_name,
+       d.dept_name 
+  FROM employees AS e 
+       INNER JOIN dept_employees AS de
+       ON e.emp_no = de.emp_no 
+       INNER JOIN departments d 
+       ON de.dept_no = d.dept_no 
+ WHERE d.dept_name = 'Sales'
+    OR d.dept_name = 'Development';
   
+
 ------------------------------------
 -- 8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
+SELECT e.last_name,
+       COUNT(e.last_name) AS count_last_name
+  FROM employees AS e
+ GROUP BY e.last_name 
+ ORDER BY count_last_name DESC 
